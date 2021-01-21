14ab6cd7dc35935067713a6d2b62b450701ffaa3
Alan Michel <73854685+Alan0170@users.noreply.github.com>
2021-01-20 22:18:06 -0300

    Add files via upload

---
+++ Verificar_Ano_Bissexto.py
@@ -0,0 +1,8 @@
+from datetime import date
+ano = int(input('Qual ano gostaria de analisar? \nPara analisar o ano atual, digite o valor 0: '))
+if ano == 0:
+    ano = date.today().year
+if ano % 4 == 0 and ano % 100 != 0 or ano % 400 == 0:
+    print('O ano {} é BISSEXTO'.format(ano))
+else:
+    print('O ano {} não é BISSEXTO'.format(ano))
